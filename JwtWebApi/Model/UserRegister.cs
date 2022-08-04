fdab961be7d3f4e405634e113696e118525504fd
Chaima OMRANE <omrane@neoledge.com>
2022-08-04 12:41:32 +0100

    first commit

---
+++ JwtWebApi/Model/UserRegister.cs
@@ -0,0 +1,9 @@
+﻿namespace JwtWebApi.Model
+{
+    public class UserRegister
+    {
+        public string Username { get; set; }= String.Empty; 
+        public string Password { get; set; }= String.Empty;
+
+    }
+}
